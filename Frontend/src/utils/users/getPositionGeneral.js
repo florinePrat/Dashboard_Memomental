ca0606741fe73b940678b98ac2a706bfed9e82d5
florine <florinepltg64@gmail>
2020-11-11 15:50:06 +0100

    utils directory

---
+++ Frontend/src/utils/users/getPositionGeneral.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import {getCookie} from "../cookies";
+
+export function getPosition (){
+    let burl = process.env.VUE_APP_API;
+    return axios({
+        method: 'get',
+        url: burl + '/api/users/positionGeneral/',
+        headers : {
+            'Content-Type': 'application/json',
+            'Authorization' : 'Bearer ' + getCookie('token')
+        }
+    })
+}
